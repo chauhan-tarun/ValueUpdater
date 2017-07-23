7dee5e4a095b505d0023b0c3f3025f2fdf9ae4ec
pchub <tarunchouhan68@gmail.com>
2017-07-23 23:05:47 +0530

    Implemented long press updates

--- app/src/main/java/com/blogspot/pchub/valueupdater/MainActivityJava.java
+++ app/src/main/java/com/blogspot/pchub/valueupdater/MainActivityJava.java
@@ -5,7 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -17,6 +19,9 @@ public class MainActivityJava extends AppCompatActivity implements View.OnClickL
     ValueUpdaterHelper updaterHelper;
     EditText et_value;
 
+    Button b_plus;
+    Button b_minus;
+
     //-----------------------------------------------------------------------------------------------------------------------------------------
 
     @Override
@@ -24,8 +29,9 @@ public class MainActivityJava extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        updaterHelper = new ValueUpdaterHelper(1,10,1);
+        updaterHelper = new ValueUpdaterHelper(1, 100, 1);
 
+        // Edit Text updates
         et_value = (EditText) findViewById(R.id.et_value);
         et_value.setText("1");
 
@@ -44,16 +50,45 @@ public class MainActivityJava extends AppCompatActivity implements View.OnClickL
             public void afterTextChanged(Editable editable) {
 
                 // Sounds good let's start our work here :P
-
-                try{
+                try {
 
                     // Get updated value
                     int updatedValue = updaterHelper.setValue(Integer.parseInt(editable.toString()));
 
-                }catch (Exception e){
+                    Log.d("MainActivityJava", "afterTextChanged: Got updated value from setValue() : " + updatedValue);
+                    if (!editable.toString().equals(String.valueOf(updatedValue))) {
+
+                        Log.d("MainActivityJava", "afterTextChanged: Updating to correct value : " + updatedValue);
+
+                        // Append new string to get the cursor at the end of EditText
+                        et_value.setText("");
+                        et_value.append(String.valueOf(updatedValue));
+                    }
+
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
 
+            }
+        });
+
+        // Buttons updates
+        b_plus = (Button) findViewById(R.id.b_plus);
+        b_minus = (Button) findViewById(R.id.b_minus);
+
+        b_plus.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                updaterHelper.getLongPressUpdates(b_plus, updaterInterface, true);
+                return true;
+            }
+        });
+
+        b_minus.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                updaterHelper.getLongPressUpdates(b_minus, updaterInterface, false);
+                return true;
             }
         });
     }
@@ -67,14 +102,30 @@ public class MainActivityJava extends AppCompatActivity implements View.OnClickL
 
         if (id == R.id.b_plus) {
 
-            et_value.setText(String.valueOf(updaterHelper.getUpdatedValue(true)));
+            // Append new string to get the cursor at the end of EditText
+            et_value.setText("");
+            et_value.append(String.valueOf(updaterHelper.getUpdatedValue(true)));
 
         } else if (id == R.id.b_minus) {
 
-            et_value.setText(String.valueOf(updaterHelper.getUpdatedValue(false)));
+            // Append new string to get the cursor at the end of EditText
+            et_value.setText("");
+            et_value.append(String.valueOf(updaterHelper.getUpdatedValue(false)));
         }
 
     }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+    ValueUpdaterHelper.ValueUpdaterInterface updaterInterface = new ValueUpdaterHelper.ValueUpdaterInterface() {
+        @Override
+        public void onValueUpdate(View view, int updatedValue) {
+
+            et_value.setText("");
+            et_value.append(String.valueOf(updatedValue));
+
+        }
+    };
 
     //-----------------------------------------------------------------------------------------------------------------------------------------
 }
--- app/src/main/java/com/blogspot/pchub/valueupdater/ValueUpdaterHelper.java
+++ app/src/main/java/com/blogspot/pchub/valueupdater/ValueUpdaterHelper.java
@@ -1,13 +1,17 @@
 package com.blogspot.pchub.valueupdater;
 
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.view.View;
+
 /**
  * Value Updater Helper will helps you to get regular updates on
  * some value whenever an event occurs(say a button clicked).
- *
  * <p>
- *     Basically with help of this class you can simply set a min value,
- *     a max value and an value value and you can get the updated value by passing
- *     the event type that is occurred.
+ * <p>
+ * Basically with help of this class you can simply set a min value,
+ * a max value and an value value and you can get the updated value by passing
+ * the event type that is occurred.
  * </p>
  */
 
@@ -18,12 +22,12 @@ public class ValueUpdaterHelper {
     /**
      * Interface for {@link ValueUpdaterHelper} which will return the value updates
      */
-    public interface ValueUpdaterInterface{
+    public interface ValueUpdaterInterface {
 
         /**
          * Interface method to get updated value
          */
-        void onValueUpdate(int updatedValue);
+        void onValueUpdate(View view, int updatedValue);
     }
 
     //-----------------------------------------------------------------------------------------------------------------------------------------
@@ -31,20 +35,40 @@ public class ValueUpdaterHelper {
     // Min, Max and Initial values
     private int min = 0, max = Integer.MAX_VALUE, value = min;
 
+    // View which is long pressed
+    private View view;
+
+    // This boolean will tell us that the view that is long pressed will increase the value or decrease the value
+    private boolean isIncreasing = false;
+
+    // ValueUpdaterInterface to return the updated values on long pres of View
+    private ValueUpdaterInterface updaterInterface;
+
+    // Speed counter to return updated values with increasing speed
+    private int updatingSpeed = 1000;
+
+    // This counter will help our updateSpeed to get reduced quickly as the updated values are sent.
+    // This will result in nice effect to user where he can see that the speed of his updates
+    // on long pressing the button is keep on increasing
+    private int counter = 0;
+
+    private Handler handler = new Handler();
+
     //-----------------------------------------------------------------------------------------------------------------------------------------
 
     /**
      * Empty constructor
      */
-    public ValueUpdaterHelper() {}
+    public ValueUpdaterHelper() {
+    }
 
     //-----------------------------------------------------------------------------------------------------------------------------------------
 
     /**
      * ValueUpdaterHelper constructor to set min, max and value value
      *
-     * @param min     Minimum value that you wants to set. By default its zero
-     * @param max     Maximum value that you wants to set. By default its Integer.MAX_VALUE
+     * @param min          Minimum value that you wants to set. By default its zero
+     * @param max          Maximum value that you wants to set. By default its Integer.MAX_VALUE
      * @param initialValue Initial value that you wants to set. By default its equal to minimum value
      */
     public ValueUpdaterHelper(int min, int max, int initialValue) {
@@ -98,10 +122,11 @@ public class ValueUpdaterHelper {
      * @return Returns the correct updated value within the defined min and max values
      */
     public int setValue(int value) {
-        if (value < min || value > max)
+        if (value < min)
             this.value = min;
-        else
+        else if (value <= max) {
             this.value = value;
+        }
 
         return this.value;
     }
@@ -115,23 +140,100 @@ public class ValueUpdaterHelper {
      * @param isIncreased boolean isIncreased.
      *                    Send true if you want increased value and
      *                    send false if you wants decreased value
-     *
      * @return The updated value within the min or max limit
      */
-    public int getUpdatedValue(boolean isIncreased){
+    public int getUpdatedValue(boolean isIncreased) {
 
-        if(isIncreased){
+        if (isIncreased) {
             // Return increased value by checking if value is less than max
-            if(value < max)
-                return ++value;
+            return setValue(value + 1);
 
-        }else{
+        } else {
             // Return decreased value by checking if value is greater than min
-            if(value > min)
-                return --value;
+            return setValue(value - 1);
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Call this method to get the updated values when a view is long pressed
+     *
+     * @param view             View which is long pressed
+     * @param updaterInterface The ValueUpdaterInterface to receive the value updates
+     *                         until the view is pressed or value is reached
+     * @param isIncreasing     boolean to define whether the value should increase or decrease.
+     *                         The value will get updated on basis of this boolean
+     */
+    public void getLongPressUpdates(@NonNull View view, @NonNull ValueUpdaterInterface updaterInterface, boolean isIncreasing) {
+        this.view = view;
+        this.updaterInterface = updaterInterface;
+        this.isIncreasing = isIncreasing;
+
+        sendLongPressUpdates();
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * This method will recall itself until the limit is reached
+     * or user releases the long pressed view.
+     * <p>
+     * This method will also send the updates using the interface with an increasing speed.
+     * As soon as user releases the long pressed view or as soon as value update limit is reached
+     * all values will get back to their default stage so that when a view is long pressed next time
+     * it gets same effect from the starting.
+     */
+    private void sendLongPressUpdates() {
+
+        if (view.isPressed()) {
+
+            // Here we returns the updated value
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    // Just to get the reference of previous value
+                    int valueBeforeUpdate = value;
+
+                    // Call setValue() to get the updatedValue
+                    if (isIncreasing) {
+                        setValue(value + 1);
+                    } else {
+                        setValue(value - 1);
+                    }
+
+                    // If we gets the updated value then call the method again to get further values
+                    // Otherwise don't call the method again and end this cycle here.
+                    if (valueBeforeUpdate != value) {
+                        updaterInterface.onValueUpdate(view, value);
+
+                        // This will result in increased speed in value updates
+                        if (updatingSpeed > 10) {
+                            updatingSpeed = -counter;
+                            counter++;
+                        }
+
+                        sendLongPressUpdates();
+                    } else {
+
+                        // So the limit is reached. Reset all long pressed values and stop sending regular updates
+                        counter = 10;
+                        updatingSpeed = 1000;
+
+                    }
+
+                }
+            }, updatingSpeed);
+
+        } else {
+
+            // View is no longer pressed. Reset all long pressed values to default and stop sending regular updates
+            counter = 10;
+            updatingSpeed = 1000;
+
         }
 
-        return value;
     }
 
     //-----------------------------------------------------------------------------------------------------------------------------------------
